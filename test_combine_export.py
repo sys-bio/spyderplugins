decab4225f9aa508306571470b575c473b830a21
0u812 <0u812@users.noreply.github.com>
2015-09-04 16:57:14 -0700

    add combine export test

---
+++ test_combine_export.py
@@ -0,0 +1,16 @@
+import tecombine as comb
+
+antimonyStr = '''
+  S1 -> S2; k1*S1
+  S1 = 10; S2 = 0
+  k1 = 1
+'''
+
+phrasedmlStr = '''
+  model1 = model "./simpleuniuni.xml"
+  sim1 = simulate uniform(0, 10, 100)
+  task1 = run sim1 on model1
+  plot time vs S1, S2
+'''
+
+comb.export('example.omex', antimonyStr, 'simpleuniuni.xml', phrasedmlStr)
