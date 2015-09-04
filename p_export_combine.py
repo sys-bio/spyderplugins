24a766c2f3bc3f381b9f2ab21615b95520008ef4
0u812 <0u812@github.com>
2015-09-04 11:34:43 -0700

    show wizard with tuxedo

--- p_export_combine.py
+++ p_export_combine.py
@@ -85,119 +85,146 @@ import string
 import tempfile, shutil, errno
 from xml.etree import ElementTree
 
-try:
-    import PyQt5
-    from PyQt5 import QtCore, QtGui, QtWidgets
-    from PyQt5.QtCore import pyqtSignal
-    from PyQt5.QtWidgets import QWizard, QWizardPage
-except ImportError:
-    pass
-
 #try:
-from spyderlib.baseconfig import get_translation
-from spyderlib.config import CONF
-from spyderlib.plugins import SpyderPluginMixin, PluginConfigPage
-from spyderlib.py3compat import getcwd, to_text_string, is_text_string
-from spyderlib.qt.QtCore import SIGNAL
-from spyderlib.qt.QtGui import QVBoxLayout, QGroupBox, QWidget, QApplication, QMessageBox
-from spyderlib.qt.compat import getopenfilenames
-from spyderlib.utils import encoding, sourcecode
-from spyderlib.utils.qthelpers import get_icon, create_action, add_actions
-from spyderlib.widgets.sourcecode.codeeditor import CodeEditor
-
-print('from ExportCombine2')
-
-_ = get_translation("p_export_combine", dirname="spyderplugins")
-
-class ExportCombine(QWidget, SpyderPluginMixin):
-    """Import a combine archive as a python script"""
-    CONF_SECTION = 'exportcombine'
-    def __init__(self, parent=None):
-        QWidget.__init__(self, parent=parent)
-        SpyderPluginMixin.__init__(self, parent)
-        print('Export combine loaded **zz** ------')
-        layout = QVBoxLayout()
-        self.setLayout(layout)
-
-        # Initialize plugin
-        self.initialize_plugin()
-
-    #------ SpyderPluginWidget API --------------------------------------------
-    def get_plugin_title(self):
-        """Return widget title"""
-        return _("Combine archive to Python exporter")
-
-#    def get_plugin_icon(self):
-#        """Return widget icon"""
-#        return get_icon('hello.png')
-
-    def get_focus_widget(self):
-        """
-        Return the widget to give focus to when
-        this plugin's dockwidget is raised on top-level
-        """
-        return None
-
-    def get_plugin_actions(self):
-        """Return a list of actions related to plugin"""
-        # Font
-        return []
-
-    def on_first_registration(self):
-        """Action to be performed on first plugin registration"""
-        self.main.tabify_plugins(self.main.inspector, self)
-        self.dockwidget.hide()
-        #pass
-
-    def register_plugin(self):
-        """Register plugin in Spyder's main window"""
-        self.connect(self, SIGNAL("edit_goto(QString,int,QString)"),
-                    self.main.editor.load)
-        self.connect(self, SIGNAL('redirect_stdio(bool)'),
-                    self.main.redirect_internalshell_stdio)
-        self.main.add_dockwidget(self)
-
-        print('Export combine register_plugin ------')
-        export_combine_act = create_action(self, _("Export COMBINE archive (.omex)"),
-                                triggered=self.export_combine)
-        export_combine_act.setEnabled(True)
-
-        add_actions(self.main.file_menu, [export_combine_act])
-        #self.main.file_menu_actions.addAction(export_combine_act)
-        #for item in self.main.file_menu_actions:
-            #try:
-                #menu_title = item.title()
-            #except AttributeError:
-                #pass
-            #else:
-                #if not is_text_string(menu_title): # string is a QString
-                    #menu_title = to_text_string(menu_title.toUtf8)
-                #print('  trying menu item {}'.format(menu_title))
-                #if item.title() == str("Import"):
-                    #item.addAction(export_combine_act)
-                    #print('    succeeded')
-                #else:
-                    #print('    failed')
-
-    def refresh_plugin(self):
-        """Refresh hello widget"""
-        pass
-
-    def closing_plugin(self, cancelable=False):
-        """Perform actions before parent main window is closed"""
-        return True
-
-    def export_combine(self):
-        pass
-
-
-print('from ExportCombine4')
-
-#==============================================================================
-# The following statements are required to register this 3rd party plugin:
-#==============================================================================
-PLUGIN_CLASS = ExportCombine
-print('from ExportCombine5')
+import PyQt5
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QWizard, QWizardPage, QLabel, QVBoxLayout
 #except ImportError:
     #pass
+
+def createSBMLPage():
+    sbmlpage = QWizardPage()
+    sbmlpage.setTitle('Select SBML file')
+
+    l = QLabel('Please select an SBML file')
+
+    layout = QVBoxLayout()
+    layout.addWidget(l)
+    sbmlpage.setLayout(layout)
+
+    return sbmlpage
+
+def createWizard():
+    w = QWizard()
+    w.addPage(createSBMLPage())
+
+    w.setWindowTitle('COMBINE Export')
+    w.show()
+    return w
+
+import sys
+
+app = QtWidgets.QApplication([])
+w = createWizard()
+sys.exit(app.exec_())
+
+try:
+    from spyderlib.baseconfig import get_translation
+    from spyderlib.config import CONF
+    from spyderlib.plugins import SpyderPluginMixin, PluginConfigPage
+    from spyderlib.py3compat import getcwd, to_text_string, is_text_string
+    from spyderlib.qt.QtCore import SIGNAL
+    from spyderlib.qt.QtGui import QVBoxLayout, QGroupBox, QWidget, QApplication, QMessageBox
+    from spyderlib.qt.QtGui import QWizard, QWizardPage
+    from spyderlib.qt.compat import getopenfilenames
+    from spyderlib.utils import encoding, sourcecode
+    from spyderlib.utils.qthelpers import get_icon, create_action, add_actions
+    from spyderlib.widgets.sourcecode.codeeditor import CodeEditor
+
+    print('from ExportCombine2')
+
+    _ = get_translation("p_export_combine", dirname="spyderplugins")
+
+    class ExportCombine(QWidget, SpyderPluginMixin):
+        """Import a combine archive as a python script"""
+        CONF_SECTION = 'exportcombine'
+        def __init__(self, parent=None):
+            QWidget.__init__(self, parent=parent)
+            SpyderPluginMixin.__init__(self, parent)
+            print('Export combine loaded **zz** ------')
+            layout = QVBoxLayout()
+            self.setLayout(layout)
+
+            # Initialize plugin
+            self.initialize_plugin()
+
+        #------ SpyderPluginWidget API --------------------------------------------
+        def get_plugin_title(self):
+            """Return widget title"""
+            return _("Combine archive to Python exporter")
+
+    #    def get_plugin_icon(self):
+    #        """Return widget icon"""
+    #        return get_icon('hello.png')
+
+        def get_focus_widget(self):
+            """
+            Return the widget to give focus to when
+            this plugin's dockwidget is raised on top-level
+            """
+            return None
+
+        def get_plugin_actions(self):
+            """Return a list of actions related to plugin"""
+            # Font
+            return []
+
+        def on_first_registration(self):
+            """Action to be performed on first plugin registration"""
+            self.main.tabify_plugins(self.main.inspector, self)
+            self.dockwidget.hide()
+            #pass
+
+        def register_plugin(self):
+            """Register plugin in Spyder's main window"""
+            self.connect(self, SIGNAL("edit_goto(QString,int,QString)"),
+                        self.main.editor.load)
+            self.connect(self, SIGNAL('redirect_stdio(bool)'),
+                        self.main.redirect_internalshell_stdio)
+            self.main.add_dockwidget(self)
+
+            print('Export combine register_plugin ------')
+            export_combine_act = create_action(self, _("Export COMBINE archive (.omex)"),
+                                    triggered=self.export_combine)
+            export_combine_act.setEnabled(True)
+
+            add_actions(self.main.file_menu, [export_combine_act])
+            #self.main.file_menu_actions.addAction(export_combine_act)
+            #for item in self.main.file_menu_actions:
+                #try:
+                    #menu_title = item.title()
+                #except AttributeError:
+                    #pass
+                #else:
+                    #if not is_text_string(menu_title): # string is a QString
+                        #menu_title = to_text_string(menu_title.toUtf8)
+                    #print('  trying menu item {}'.format(menu_title))
+                    #if item.title() == str("Import"):
+                        #item.addAction(export_combine_act)
+                        #print('    succeeded')
+                    #else:
+                        #print('    failed')
+
+        def refresh_plugin(self):
+            """Refresh hello widget"""
+            pass
+
+        def closing_plugin(self, cancelable=False):
+            """Perform actions before parent main window is closed"""
+            return True
+
+        def export_combine(self):
+            pass
+
+
+    print('from ExportCombine4')
+
+    #==============================================================================
+    # The following statements are required to register this 3rd party plugin:
+    #==============================================================================
+    PLUGIN_CLASS = ExportCombine
+    print('from ExportCombine5')
+except ImportError:
+    pass
 
